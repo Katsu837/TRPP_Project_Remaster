df26d4d40cd56e65a07f50672da5116f403ab04f
Pablo <parusov.pashka@ya.ru>
2023-04-29 16:39:03 +0300

    any changes

--- src/components/registration/Registration.jsx
+++ src/components/registration/Registration.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import "../../styles/Registration.css"
 
 const Registration = function(){
     const [user, setUser] = useState({
